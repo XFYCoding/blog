80550f694d4ba807ec4750fc19457e94766b0d2d
xfycoding <508591736@qq.com>
2023-05-10 23:46:08 +0800

    评论

--- docs/.vitepress/theme/components/Comment.vue
+++ docs/.vitepress/theme/components/Comment.vue
@@ -4,13 +4,13 @@
   </div>
 </template>
 <script>
-import md5 from "md5";
-import Gitalk from "gitalk";
-import "gitalk/dist/gitalk.css";
+import md5 from "md5"
+import Gitalk from "gitalk"
+import "gitalk/dist/gitalk.css"
 export default {
   name: "git-talk",
   data() {
-    return {};
+    return {}
   },
   mounted() {
     const commentConfig = {
@@ -18,17 +18,18 @@ export default {
       clientSecret: "1bf108cfa63e82a5bdd03c9b7fbc711628a9e1e3",
       repo: "blog",
       owner: "xfycoding",
+      admin: ["xfycoding"],
       id: md5(location.pathname),
       distractionFreeMode: false,
-    };
-    const gitalk = new Gitalk(commentConfig);
-    gitalk.render("gitalk-container");
+    }
+    const gitalk = new Gitalk(commentConfig)
+    gitalk.render("gitalk-container")
   },
-};
+}
 </script>
 <style>
 .gt-container .gt-header-textarea {
-  margin-top: 10px;
+    margin-top: 10px;
   color: #000;
 }
 </style>
