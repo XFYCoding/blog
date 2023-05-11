2094235a20c3bec32a8f75e282d81509a660dd47
xfycoding <508591736@qq.com>
2023-05-11 22:24:56 +0800

    新增修改页面

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -45,58 +45,62 @@ module.exports = {
       prev: "上一篇",
       next: "下一篇",
     },
+    editLink: {
+      pattern: 'https://github.com/xfycoding/blog/edit/master/docs/:path',
+      text:"编辑此页"
+    },
     //algolia搜索配置
     search: {
-        provider: "algolia",
-        options: {
-          appId: "VSH7Y698A0",
-          apiKey: "b4419120f46dabfc02163b4fab75491e",
-          indexName: "xfycodingblog",
-          locales: {
-            zh: {
-              placeholder: "搜索文档",
-              translations: {
-                button: {
-                  buttonText: "搜索文档",
-                  buttonAriaLabel: "搜索文档",
+      provider: "algolia",
+      options: {
+        appId: "VSH7Y698A0",
+        apiKey: "b4419120f46dabfc02163b4fab75491e",
+        indexName: "xfycodingblog",
+        locales: {
+          zh: {
+            placeholder: "搜索文档",
+            translations: {
+              button: {
+                buttonText: "搜索文档",
+                buttonAriaLabel: "搜索文档",
+              },
+              modal: {
+                searchBox: {
+                  resetButtonTitle: "清除查询条件",
+                  resetButtonAriaLabel: "清除查询条件",
+                  cancelButtonText: "取消",
+                  cancelButtonAriaLabel: "取消",
                 },
-                modal: {
-                  searchBox: {
-                    resetButtonTitle: "清除查询条件",
-                    resetButtonAriaLabel: "清除查询条件",
-                    cancelButtonText: "取消",
-                    cancelButtonAriaLabel: "取消",
-                  },
-                  startScreen: {
-                    recentSearchesTitle: "搜索历史",
-                    noRecentSearchesText: "没有搜索历史",
-                    saveRecentSearchButtonTitle: "保存至搜索历史",
-                    removeRecentSearchButtonTitle: "从搜索历史中移除",
-                    favoriteSearchesTitle: "收藏",
-                    removeFavoriteSearchButtonTitle: "从收藏中移除",
-                  },
-                  errorScreen: {
-                    titleText: "无法获取结果",
-                    helpText: "你可能需要检查你的网络连接",
-                  },
-                  footer: {
-                    selectText: "选择",
-                    navigateText: "切换",
-                    closeText: "关闭",
-                    searchByText: "搜索提供者",
-                  },
-                  noResultsScreen: {
-                    noResultsText: "无法找到相关结果",
-                    suggestedQueryText: "你可以尝试查询",
-                    reportMissingResultsText: "你认为该查询应该有结果？",
-                    reportMissingResultsLinkText: "点击反馈",
-                  },
+                startScreen: {
+                  recentSearchesTitle: "搜索历史",
+                  noRecentSearchesText: "没有搜索历史",
+                  saveRecentSearchButtonTitle: "保存至搜索历史",
+                  removeRecentSearchButtonTitle: "从搜索历史中移除",
+                  favoriteSearchesTitle: "收藏",
+                  removeFavoriteSearchButtonTitle: "从收藏中移除",
+                },
+                errorScreen: {
+                  titleText: "无法获取结果",
+                  helpText: "你可能需要检查你的网络连接",
+                },
+                footer: {
+                  selectText: "选择",
+                  navigateText: "切换",
+                  closeText: "关闭",
+                  searchByText: "搜索提供者",
+                },
+                noResultsScreen: {
+                  noResultsText: "无法找到相关结果",
+                  suggestedQueryText: "你可以尝试查询",
+                  reportMissingResultsText: "你认为该查询应该有结果？",
+                  reportMissingResultsLinkText: "点击反馈",
                 },
               },
             },
           },
         },
       },
+    },
     // 自定义扩展区
     //文章元数据配置
     articleMetadataConfig: {
@@ -120,5 +124,5 @@ module.exports = {
         return htmlResult;
       };
     },
-  }
+  },
 };
